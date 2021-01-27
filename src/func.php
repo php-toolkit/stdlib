bff732354acda34692fa7754762e67cc38555361
inhere <in.798@qq.com>
2021-01-27 17:36:04 +0800

    udpate global func

--- src/func.php
+++ src/func.php
@@ -21,27 +21,6 @@ if (!function_exists('vdump')) {
     }
 }
 
-if (!function_exists('pdump')) {
-    /**
-     * Dump data like var_dump
-     *
-     * @param mixed ...$vars
-     */
-    function pdump(...$vars)
-    {
-        $trace = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS, 2);
-
-        $line = $trace[0]['line'];
-        $pos  = $trace[1]['class'] ?? $trace[0]['file'];
-
-        if ($pos) {
-            echo "CALL ON $pos($line):\n";
-        }
-
-        echo Toolkit\Stdlib\Php::printVars(...$vars), PHP_EOL;
-    }
-}
-
 if (!function_exists('edump')) {
     /**
      * Dump data like var_dump, will call exit() on print after.
@@ -83,5 +62,47 @@ if (!function_exists('ddump')) {
 
         echo Toolkit\Stdlib\Php::dumpVars(...$vars), PHP_EOL;
         die(0);
+    }
+}
+
+if (!function_exists('pprints')) {
+    /**
+     * Print data use print_r()
+     *
+     * @param mixed ...$vars
+     */
+    function pprint(...$vars)
+    {
+        $trace = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS, 2);
+
+        $line = $trace[0]['line'];
+        $pos  = $trace[1]['class'] ?? $trace[0]['file'];
+
+        if ($pos) {
+            echo "CALL ON $pos($line):\n";
+        }
+
+        echo Toolkit\Stdlib\Php::printVars(...$vars), PHP_EOL;
+    }
+}
+
+if (!function_exists('eprints')) {
+    /**
+     * Print data use export_var()
+     *
+     * @param mixed ...$vars
+     */
+    function eprints(...$vars)
+    {
+        $trace = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS, 2);
+
+        $line = $trace[0]['line'];
+        $pos  = $trace[1]['class'] ?? $trace[0]['file'];
+
+        if ($pos) {
+            echo "CALL ON $pos($line):\n";
+        }
+
+        echo Toolkit\Stdlib\Php::exportVar(...$vars), PHP_EOL;
     }
 }
