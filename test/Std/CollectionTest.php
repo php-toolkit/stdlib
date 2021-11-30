c34adb3c13772d460ba934b6562b39c539312f07
Inhere <in.798@qq.com>
2021-11-30 23:01:56 +0800

    chore: add some tests for std/collection

---
+++ test/Std/CollectionTest.php
@@ -0,0 +1,24 @@
+<?php declare(strict_types=1);
+
+namespace Toolkit\StdlibTest\Std;
+
+use Toolkit\Stdlib\Std\Collection;
+use Toolkit\StdlibTest\BaseLibTestCase;
+
+/**
+ * class CollectionTest
+ *
+ * @author inhere
+ */
+class CollectionTest extends BaseLibTestCase
+{
+    public function testCollection_basic(): void
+    {
+        $c = Collection::new(['age' => 23, 'name' => 'inhere']);
+
+        $this->assertTrue($c->has('age'));
+        $this->assertEquals(23, $c->get('age'));
+        $this->assertEquals(23, $c->getInt('age'));
+        $this->assertEquals('23', $c->getString('age'));
+    }
+}
